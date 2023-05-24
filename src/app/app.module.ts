6079a9f497354bff1243b99fbecdbabecba36f43
Saddam BEN KHALED <saddambenkhaled@gmail.com>
2023-05-25 01:22:14 +0200

    components creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskCreateComponent } from './task-create/task-create.component';
+import { TaskEditComponent } from './task-edit/task-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskCreateComponent,
+    TaskEditComponent
   ],
   imports: [
     BrowserModule
